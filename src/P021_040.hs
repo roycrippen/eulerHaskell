f293fa7f9c994d63685cd709a0331776d8008078
roycrippen <roycrippen@gmail.com>
2017-03-05 01:13:20 -0600

    clean up

--- src/P021_040.hs
+++ src/P021_040.hs
@@ -12,7 +12,7 @@ import           Data.List
 import           Data.Numbers.Primes
 import           Data.Ratio
 
-
+------------------------------------------------------------------
 -- | Euler 021: Amicable numbers.
 p021 :: IO ()
 p021 = do
@@ -28,7 +28,7 @@ isAmicable n = v /= n && sumFactors v == n
 amicable :: Integer -> Integer
 amicable n = sum $ filter isAmicable [2,4..n]
 
-
+------------------------------------------------------------------
 -- | Euler 022: Names scores.
 p022 :: IO ()
 p022 = do
@@ -42,7 +42,6 @@ p022 = do
 -- score each name in list by index
 scoreName :: String -> Int
 scoreName = sum . toIntChars
-
 
 -- > Euler 023: Non-abundant sums.
 p023 :: IO ()
@@ -71,12 +70,13 @@ isAbundantSum n = any (\x -> arr U.! (n - x)) (takeWhile (<= n `div` 2) abundant
 nonAbundants :: Int -> [Int]
 nonAbundants n = filter(not . isAbundantSum) [1..n]
 
-
+------------------------------------------------------------------
 -- > Euler 024:
 p024 :: IO ()
 p024 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p024"
+
 
 -- > Euler 025:
 p025 :: IO ()
@@ -84,7 +84,7 @@ p025 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p025"
 
-
+------------------------------------------------------------------
 -- > Euler 026:
 p026 :: IO ()
 p026 = do
@@ -98,98 +98,98 @@ p027 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p027"
 
-
+------------------------------------------------------------------
 -- > Euler 028:
 p028 :: IO ()
 p028 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p028"
 
-
+------------------------------------------------------------------
 -- > Euler 029:
 p029 :: IO ()
 p029 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p029"
 
-
+------------------------------------------------------------------
 -- > Euler 030:
 p030 :: IO ()
 p030 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p030"
 
-
+------------------------------------------------------------------
 -- > Euler 031:
 p031 :: IO ()
 p031 = do
     let res = 0
     putStrLn $ assertEq res 0 "p031"
 
-
+------------------------------------------------------------------
 -- > Euler 032:
 p032 :: IO ()
 p032 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p032"
 
-
+------------------------------------------------------------------
 -- > Euler 033:
 p033 :: IO ()
 p033 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p033"
 
-
+------------------------------------------------------------------
 -- > Euler 034:
 p034 :: IO ()
 p034 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p034"
 
-
+------------------------------------------------------------------
 -- > Euler 035:
 p035 :: IO ()
 p035 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p035"
 
-
+------------------------------------------------------------------
 -- > Euler 036:
 p036 :: IO ()
 p036 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p036"
 
-
+------------------------------------------------------------------
 -- > Euler 037:
 p037 :: IO ()
 p037 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p037"
 
-
+------------------------------------------------------------------
 -- > Euler 038:
 p038 :: IO ()
 p038 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p038"
 
-
+------------------------------------------------------------------
 -- > Euler 039:
 p039 :: IO ()
 p039 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p039"
 
-
+------------------------------------------------------------------
 -- > Euler 030:
 p040 :: IO ()
 p040 = do
     let res  = 0
     putStrLn $ assertEq res 0 "p040"
 
-
+------------------------------------------------------------------
 -- > List of project Euler functions.
 solutionsP021_040 :: [IO()]
 solutionsP021_040 =
